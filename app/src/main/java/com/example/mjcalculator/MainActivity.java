2c09783906ff5c8893e6cb11fe43d438916c52d4
Mateusz Janczak <mateuszjanczak.mj@gmail.com>
2020-03-24 02:02:35 +0100

    icon, font and colors changed

--- app/src/main/java/com/example/mjcalculator/MainActivity.java
+++ app/src/main/java/com/example/mjcalculator/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.mjcalculator;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
